b3327527577b849d558a2fdb557d77781b0e27d8
Vaibhav Limbachiya <140944038+vaibhavsl1810@users.noreply.github.com>
2024-12-15 12:04:04 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+const display = document.getElementById('display');
+
+        function appendValue(value) {
+            display.value += value;
+        }
+
+        function clearDisplay() {
+            display.value = '';
+        }
+
+        function calculateResult() {
+            try {
+                display.value = eval(display.value);
+            } catch (e) {
+                alert('Invalid expression');
+                clearDisplay();
+            }
+        }
